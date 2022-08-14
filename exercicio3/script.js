245a0d188e5f09286a14cd31b680e503290991c0
izabela08 <izabelalima17@gmail.com>
2022-08-14 15:39:38 -0300

    d-izabela-lima-aulao12i

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,24 @@
-let nacionalidade //= adicione o prompt aqui
+// if(media >= 5){
+//     console.log('APROVADO')
+// }else if(media >= 3){
+//     console.log('REPROVADO')
+// }else if(media < 3){
+//     console.log('RECUPERAÇÃO')
+// }
+// else{
+//     console.log("DADOS INVALIDOS")
+// }
+let nacionalidade = prompt('QUAL SUA NACIONALIDADE?').toLowerCase()
+ if(nacionalidade ==='brasileira'){
+   console.log(nacionalidade)
+ }else if(nacionalidade === 'argentina'){
+   console.log(nacionalidade)
+ }else if(nacionalidade === 'uruguaia'){
+   console.log(nacionalidade)
+ }else if(nacionalidade === 'chilena'){
+   console.log(nacionalidade)
+ }else if(nacionalidade === 'colombiana'){
+   console.log(nacionalidade)
+ }else{
+    console.log("NÃO CONSTA")
+ }
